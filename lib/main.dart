55a48686a9733ab2835ad4d68d17112149856822
Lonewolf230 <manish2306j@gmail.com>
2024-12-29 18:13:51 +0530

    Home Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:pdftool/screens/auth_screen.dart';
 import 'package:pdftool/screens/home_screen.dart';
 import 'package:flutter/services.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:pdftool/screens/loading_screen.dart';
+import 'firebase_options.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await GoogleSignIn().signInSilently();
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   runApp(const MyApp());
 }
@@ -15,11 +23,25 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'PDF Tool',
-      theme: ThemeData().copyWith(
-          colorScheme: ColorScheme.fromSeed(
-              seedColor: const Color.fromARGB(255, 0, 253, 187))),
-      home: const HomeScreen(),
-    );
+        title: 'PDF Tool',
+        theme: ThemeData().copyWith(
+            colorScheme: ColorScheme.fromSeed(
+                seedColor: const Color.fromARGB(255, 0, 253, 187))),
+        home: StreamBuilder(
+            stream: FirebaseAuth.instance.authStateChanges(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return LoadingScreen();
+              }
+              if (snapshot.hasData) {
+                return const HomeScreen();
+              }
+              if (snapshot.hasError) {
+                return Center(
+                  child: Text('Failed to initialise App'),
+                );
+              }
+              return const AuthScreen();
+            }));
   }
 }
